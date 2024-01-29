0bcefe80c03385caeecfc08bcb5247469bb053ef
abdusattoryaminjonov <abdusattoryaminjonov@gmail.com>
2024-01-29 16:26:38 +0500

    initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Next Gen demo-1"),
+    );
+  }
+}
